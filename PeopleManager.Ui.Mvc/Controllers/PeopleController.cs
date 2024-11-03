8eb722fffb866cc64b32cc939922ed9cd805ed5d
ThiboVerstraete <146202098+ThiboVerstraete@users.noreply.github.com>
2024-11-03 23:18:49 +0100

    Initial commit

---
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Ui.Mvc.Core;
+using PeopleManager.Ui.Mvc.Models;
+
+namespace PeopleManager.Ui.Mvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+
+        private readonly PeopleManagerDbContext _peopleManagerDbContext;
+
+        public PeopleController(PeopleManagerDbContext peopleManagerDbContext)
+        {
+            _peopleManagerDbContext = peopleManagerDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var people = _peopleManagerDbContext.People.ToList();
+
+            return View(people);
+        }
+    }
+}
