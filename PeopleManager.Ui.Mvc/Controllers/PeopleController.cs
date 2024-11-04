54cf35212326be06c1a61f2c8427c3e7afbd5333
ThiboVerstraete <146202098+ThiboVerstraete@users.noreply.github.com>
2024-11-04 10:47:29 +0100

    eind les 6

--- PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -37,5 +37,36 @@ namespace PeopleManager.Ui.Mvc.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var person = _peopleManagerDbContext.People.FirstOrDefault(x => x.Id == id);
+            if (person == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Person person)
+        {
+            var dbperson = _peopleManagerDbContext.People.FirstOrDefault(x => x.Id == id);
+            if (dbperson == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            dbperson.FirstName = person.FirstName;
+            dbperson.LastName = person.LastName;
+            dbperson.Email = person.Email;
+
+            _peopleManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
